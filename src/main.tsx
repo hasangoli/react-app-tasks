f20cfa19c9f539f64a784f578b2f1dea33f88fca
Hasan Goli <mrhasangoli@gmail.com>
2025-05-14 21:29:47 +0330

    Tailwind and react-router configured

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<BrowserRouter>
+		<Routes>
+			<Route index element={<App />} />
+		</Routes>
+	</BrowserRouter>
+);
